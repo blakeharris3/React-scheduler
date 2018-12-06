e75a3b95793c34c03ed63c1671e66987c51547bc
Blake Harris <Blake@Blakes-MBP.local>
2018-12-06 10:11:19 -0600

    redirecturi update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
-// import { BrowserRouter } from 'react-router-dom';
 import Auth from './Auth'
 
 
