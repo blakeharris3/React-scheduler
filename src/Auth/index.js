86edb06244928c23f5f20ab26734f57897481543
Blake Harris <Blake@Blakes-MBP.local>
2018-12-06 11:17:21 -0600

    redirecturi update

--- src/Auth/index.js
+++ src/Auth/index.js
@@ -9,7 +9,7 @@ const LOGIN_FAILURE_PAGE = "/"
 export default class Auth {
     auth0 = new auth0.WebAuth({
         domain: "scheduler-3.auth0.com",
-        clientID: "u9KOK9WmpJPC3ABdHqtQ-gTvwG04f4bs",
+        clientID: "yuC1V2frSFtdf2eGm1TVpv2Rdyk8JqJ5",
         redirectUri: "https://thescheduler.netlify.com/callback",
         audience: "https://scheduler-3.auth0.com/userinfo",
         responseType: "token id_token",
