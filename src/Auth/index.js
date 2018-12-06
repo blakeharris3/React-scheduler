4dcf6d49d7d028a6584655fd57293a1735d8d21a
Blake Harris <Blake@Blakes-MBP.local>
2018-12-06 10:13:28 -0600

    redirecturi update

--- src/Auth/index.js
+++ src/Auth/index.js
@@ -10,8 +10,7 @@ export default class Auth {
     auth0 = new auth0.WebAuth({
         domain: "scheduler-3.auth0.com",
         clientID: "u9KOK9WmpJPC3ABdHqtQ-gTvwG04f4bs",
-        redirectUri: "http://thescheduler.netlify.com/callback",
-        redirectUri: 'https://thescheduler.netlify.com',
+        redirectUri: "https://thescheduler.netlify.com/callback",
         audience: "https://scheduler-3.auth0.com/userinfo",
         responseType: "token id_token",
         scope: "openid profile"
